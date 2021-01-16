d0519192c722f263e9b6a46f1bd9af96c5d44a04
AmjadMoqade98 <amjadmoqade98@gmail.com>
2021-01-16 13:53:51 +0200

    - refactoring - solving bugs

--- src/Rejexes.java
+++ src/Rejexes.java
@@ -1,10 +1,4 @@
-/**
- * Constant class for the used rejex in the program
- */
 public class Rejexes {
-    final static String INT = "[0-9][0-9]*";
-    final static String FLOAT = "[0-9][0-9]*(\\.([0-9])+)?";
     final static String LIBRARIES = "(#include((<[^>]+>));)*";
     final static String NAME = "[a-zA-Z][a-zA-Z0-9]*";
-    final static String NAME_LIST = "([a-zA-Z][a-zA-Z0-9]*)(,[a-zA-Z][a-zA-Z0-9]*)*";
 }
